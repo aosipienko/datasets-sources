19be01b47caf689a8d73dc88789af1fc535efdc3
Andrzej Osipienko <andrzej.osipienko@hobly.pl>
2023-07-12 18:09:52 +0200

    split knf and mswia sanctinos

--- startup.sh
+++ startup.sh
@@ -3,8 +3,6 @@
 set -x
 
 cat datasets/pl_pep*.json | grep -v '^$' | sort | uniq > datasets/pl_peps.entities.ftm.json
-cat datasets/pl_knf_warnings.entities.ftm.json \
-    datasets/pl_mswia_sanctions.entities.ftm.json | grep -v '^$' > datasets/pl_sanctions.entities.ftm.json
 
 docker-compose up -d
 docker-compose logs -f --tail 200
