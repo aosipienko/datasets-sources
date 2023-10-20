0fed312fb5a42fdb055fcd17c0e3ae701a997232
Andrzej Osipienko <andrzej.osipienko@hobly.pl>
2023-10-20 16:03:12 +0000

    fix startup when pl_peps... exists

--- startup.sh
+++ startup.sh
@@ -2,10 +2,9 @@
 
 set -x
 
-rm datasets/pl_pep.entities.ftm.json
+rm datasets/pl_peps.entities.ftm.json
 
-cat datasets/pl_pep*.json | grep -v '^$' | sort | uniq > datasets/pl_peps.entities.ftm.json
+cat datasets/pl_pep*.json | grep -v '^$' | sort | uniq >datasets/pl_peps.entities.ftm.json
 
 docker-compose up -d
 docker-compose logs -f --tail 200
-
